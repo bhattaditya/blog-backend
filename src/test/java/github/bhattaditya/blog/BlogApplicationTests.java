dc874ec907eaaa20704799d326f71b9f51248dcd
Aditya Bhatt <bhattaditya2@gmail.com>
2022-05-24 18:34:50 +0530

    initial commit

---
+++ src/test/java/github/bhattaditya/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package github.bhattaditya.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
